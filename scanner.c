e82d9c0f7be3c2c8f8c844ef184657e1ed8e64f2
randycanegaly <canegaly@gmail.com>
2025-04-18 18:12:48 -0400

    Finished implementing Chapter 16. I'm not sure it is working correctly. What the scanner prints out doesn't look right. TO DO: Next session, enter a few single lexemes at the command line and in gdb step through what gets printed out. Check if it is working correctly.

--- scanner.c
+++ scanner.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
@@ -18,6 +19,12 @@ void initScanner(const char *source) {
   scanner.current = source;
   scanner.line = 1;
 }
+
+static bool isAlpha(char c) {
+  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
+
+static bool isDigit(char c) { return c >= '0' && c <= '9'; };
 
 static bool isAtEnd() {
   return *scanner.current == '\0'; // source is a well-terminated string
@@ -104,6 +111,104 @@ static void skipWhiteSpace() {
   }
 }
 
+static TokenType checkKeyword(int start, int length, const char *rest,
+                              TokenType type) {
+  if (scanner.current - scanner.start == start + length &&
+      memcmp(scanner.start + start, rest, length) == 0) {
+    return type;
+  }
+
+  return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifierType() {
+  switch (scanner.start[0]) {
+  case 'a':
+    return checkKeyword(1, 2, "nd", TOKEN_AND);
+  case 'c':
+    return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+  case 'e':
+    return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+  case 'f':
+    if (scanner.current - scanner.start > 1) {
+      switch (scanner.start[1]) { // check the second character in the lexeme
+      case 'a':
+        return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+      case 'o':
+        return checkKeyword(2, 1, "r", TOKEN_OR);
+      case 'u':
+        return checkKeyword(2, 1, "n", TOKEN_FUN);
+      }
+    }
+    break; // current is not more than 1 character past start, no match
+  case 'i':
+    return checkKeyword(1, 1, "f", TOKEN_IF);
+  case 'n':
+    return checkKeyword(1, 2, "il", TOKEN_NIL);
+  case 'o':
+    return checkKeyword(1, 1, "r", TOKEN_OR);
+  case 'p':
+    return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+  case 'r':
+    return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+  case 's':
+    return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+  case 't':
+    if (scanner.current - scanner.start > 1) {
+      switch (scanner.start[1]) {
+      case 'h':
+        return checkKeyword(2, 2, "is", TOKEN_THIS);
+      case 'r':
+        return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+      }
+    }
+    break;
+  case 'v':
+    return checkKeyword(1, 2, "ar", TOKEN_VAR);
+  case 'w':
+    return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+  }
+
+  return TOKEN_IDENTIFIER;
+}
+
+static Token identifier() {
+  while (isAlpha(peek()) || isDigit(peek()))
+    advance();
+  return makeToken(identifierType());
+}
+
+static Token number() {
+  while (isDigit(peek()))
+    advance();
+
+  // look for fractional part
+  if (peek() == '.' && isdigit(peekNext())) {
+    // Consume the '.'
+    advance();
+
+    while (isDigit(peek()))
+      advance();
+  }
+
+  return makeToken(TOKEN_NUMBER);
+}
+
+static Token string() {
+  while (peek() != '"' && !isAtEnd()) {
+    if (peek() == '\n')
+      scanner.line++;
+    advance();
+  }
+
+  if (isAtEnd())
+    return errorToken("Unterminated string.");
+
+  // the closing quote
+  advance();
+  return makeToken(TOKEN_STRING);
+}
+
 Token scanToken() {
   skipWhiteSpace();
   scanner.start = scanner.current;
@@ -112,6 +217,10 @@ Token scanToken() {
     return makeToken(TOKEN_EOF);
 
   char c = advance();
+  if (isAlpha(c))
+    return identifier();
+  if (isdigit(c))
+    return number();
 
   switch (c) {
   case '(':
@@ -144,6 +253,8 @@ Token scanToken() {
     return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
   case '>':
     return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+  case '"':
+    return string();
   }
 
   return errorToken("Unexpected character.");
