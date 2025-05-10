835f5497619ce0ee04063d2eb43f079e93000a09
randycanegaly <canegaly@gmail.com>
2025-05-09 21:19:39 -0400

    Finished section 18.2.

--- value.h
+++ value.h
@@ -17,7 +17,18 @@ typedef struct {
   } as;
 } Value;
 
-#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value)                                                         \
+  ((value).as.boolean) // take a Value type and return the boolean of its union
+#define AS_NUMBER(value) ((value).as.number)
+
+#define BOOL_VAL(value)                                                        \
+  ((Value){VAL_BOOL,                                                           \
+           {.boolean = value}}) // take C type and create a Value with the
+                                // correct type and union member type
 #define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
