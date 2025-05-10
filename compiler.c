231c5c73577c11c1464a95b1abc70b2101056df2
randycanegaly <canegaly@gmail.com>
2025-05-09 21:26:01 -0400

    Finished section 18.3, up to section 18.3.1.

--- compiler.c
+++ compiler.c
@@ -8,6 +8,7 @@
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
+#include "value.h"
 
 #ifdef DEBUG_PRINT_CODE
 #include "debug.h"
@@ -163,7 +164,7 @@ static void grouping() {
 
 static void number() {
   double value = strtod(parser.previous.start, NULL);
-  emitConstant(value);
+  emitConstant(NUMBER_VAL(value));
 }
 
 static void unary() {
--- value.h
+++ value.h
@@ -21,14 +21,10 @@ typedef struct {
 #define IS_NIL(value) ((value).type == VAL_NIL)
 #define IS_NUMBER(value) ((value).type == VAL_NUMBER)
 
-#define AS_BOOL(value)                                                         \
-  ((value).as.boolean) // take a Value type and return the boolean of its union
+#define AS_BOOL(value) ((value).as.boolean)
 #define AS_NUMBER(value) ((value).as.number)
 
-#define BOOL_VAL(value)                                                        \
-  ((Value){VAL_BOOL,                                                           \
-           {.boolean = value}}) // take C type and create a Value with the
-                                // correct type and union member type
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
